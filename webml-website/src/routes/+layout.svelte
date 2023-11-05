752e22f3f2c60fa62399cee1467ac4b943205ba4
Francois Lancelot <16709438+francoislanc@users.noreply.github.com>
2023-11-05 22:21:53 +0100

    Add webml website

---
+++ webml-website/src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import '../app.postcss';
+
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { AppBar, storePopup } from '@skeletonlabs/skeleton';
+	import { AppShell } from '@skeletonlabs/skeleton';
+
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+</script>
+
+<head>
+	<title>WebML - Machine Learning in the browser</title>
+</head>
+
+<AppShell>
+	<svelte:fragment slot="header"
+		><AppBar><h2 class="h2">WebML - Machine Learning in the browser</h2></AppBar></svelte:fragment
+	>
+	<slot />
+</AppShell>
---
+++ webml-website/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5">
+		<div class="w-full grid grid-cols-1 gap-4">
+			<a class="card bg-initial card-hover overflow-hidden" href="#top"
+				><header>
+					<img
+						src="webml-speech-recognition-screenshot.png"
+						class=""
+						alt="WebML - Speech recognition"
+					/>
+				</header>
+				<div class="p-4 space-y-4">
+					<article>
+						<p>ML-powered speech recognition in your browser.</p>
+					</article>
+				</div>
+			</a>
+		</div>
+	</div>
+</div>
