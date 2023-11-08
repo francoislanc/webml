e31fded81199e1e6da7ead910b69479fd809471a
Francois Lancelot <16709438+francoislanc@users.noreply.github.com>
2023-11-08 22:55:04 +0100

    Add blip model

--- webml-rust/build.sh
+++ webml-rust/build.sh
@@ -3,3 +3,4 @@
 rm -r $(dirname "$0")/build
 cargo build --target wasm32-unknown-unknown --release
 wasm-bindgen target/wasm32-unknown-unknown/release/whisper.wasm --out-dir $(dirname "$0")/build --target web
+wasm-bindgen target/wasm32-unknown-unknown/release/blip.wasm --out-dir $(dirname "$0")/build --target web
--- webml-rust/src/worker.rs
+++ webml-rust/src/worker.rs
@@ -16,13 +16,6 @@ extern "C" {
     pub fn log(s: &str);
 }
 
-#[macro_export]
-macro_rules! console_log {
-    // Note that this is using the `log` function imported above during
-    // `bare_bones`
-    ($($t:tt)*) => ($crate::worker::log(&format_args!($($t)*).to_string()))
-}
-
 pub const DTYPE: DType = DType::F32;
 
 pub enum Model {
