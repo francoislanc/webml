e899463585790009714fbafc38fcb521f99e78e5
Francois Lancelot <16709438+francoislanc@users.noreply.github.com>
2023-12-02 17:09:37 +0100

    Add github link

--- webml-website/src/routes/+layout.svelte
+++ webml-website/src/routes/+layout.svelte
@@ -5,6 +5,7 @@
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { AppBar, storePopup } from '@skeletonlabs/skeleton';
 	import { AppShell } from '@skeletonlabs/skeleton';
+	import Github from '~icons/mdi/github';
 
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
@@ -15,7 +16,14 @@
 
 <AppShell>
 	<svelte:fragment slot="header"
-		><AppBar><h2 class="h2">WebML - Machine Learning in the browser</h2></AppBar></svelte:fragment
+		><AppBar
+			><h2 class="h2">WebML - Machine Learning in the browser</h2>
+			<a
+				target="_blank"
+				href="https://github.com/francoislanc/webml"
+				class="mt-5 btn-icon variant-ghost"><Github /></a
+			></AppBar
+		></svelte:fragment
 	>
 	<slot />
 </AppShell>
