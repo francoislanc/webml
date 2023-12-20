205d3819eda1d4d9483483319b1426ea2bc6027b
Francois Lancelot <16709438+francoislanc@users.noreply.github.com>
2023-12-20 15:06:22 +0100

    Remove scripts used during debug

--- browser-extensions/webml-image-captioning/scripts/export_build_folder_to_windows.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-
-rm -rf /mnt/c/Users/lance/Documents/Code/p3nguknight/webml/image-captioning
-cp -R $(dirname "$0")/../dist /mnt/c/Users/lance/Documents/Code/p3nguknight/webml/image-captioning
--- browser-extensions/webml-speech-recognition/scripts/export_build_folder_to_windows.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-
-rm -rf /mnt/c/Users/lance/Documents/Code/p3nguknight/webml/stt-extension
-cp -R $(dirname "$0")/../dist /mnt/c/Users/lance/Documents/Code/p3nguknight/webml/stt-extension
